9838435d138c69c1f2968fbf686bbf990ce24acc
Lola García <mariadolores.garcia@bosonit.com>
2023-05-12 11:38:14 +0200

    portfolio menu fixed and JS exercises in progress

---
+++ 04.JAVASCRIPT/01BasicJS/58BooleanValues.js
@@ -0,0 +1,16 @@
+/*Otro tipo de datos es el booleano . Los valores booleanos solo pueden ser uno de dos valores: trueo false. Son básicamente pequeños interruptores de encendido y apagado, donde trueestá encendido y falseapagado. Estos dos estados son mutuamente excluyentes.
+
+Nota: los valores booleanos nunca se escriben entre comillas. Las cadenas "true"y "false"no son booleanas y no tienen un significado especial en JavaScript.
+
+Modifique la welcomeToBooleansfunción para que devuelva trueen lugar de false.*/
+
+
+function welcomeToBooleans() {
+    // Only change code below this line
+    return true; // Change this line
+    // Only change code above this line
+}
+
+//llamada a la funcion:
+    
+welcomeToBooleans();
